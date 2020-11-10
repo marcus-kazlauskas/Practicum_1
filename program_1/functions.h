4c25ad224cd2de677563fb303c40a42f6bdafe6f
Mikhail Kozlov <kozlov.mihael@yandex.ru>
2020-11-10 23:19:52 +0300

    Readme, task and programm description added

--- program_1/functions.h
+++ program_1/functions.h
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-double norm1(double *y2, double *y1){
+double norm1(double *y2, double *y1){       // p-норма при p=infinity, используется для определения точности численного решения
     double max = fabs(y2[0]-y1[0]);
     
     for (int i = 1; i <= 10; i++){
@@ -24,7 +24,7 @@ double norm1(double *y2, double *y1){
     return max;
 }
 
-double norm2(double *y2, double *y1){
+double norm2(double *y2, double *y1){       // манхэттенское расстояние, в задаче не используется
     double sum = 0;
     
     for (int i = 1; i <= 10; i++){
@@ -34,7 +34,7 @@ double norm2(double *y2, double *y1){
     return sum;
 }
 
-double norm3(double *y2, double *y1){
+double norm3(double *y2, double *y1){       // евклидова норма, в задаче не используется
     double sum = 0;
     
     for (int i = 1; i <= 10; i++){
@@ -44,159 +44,159 @@ double norm3(double *y2, double *y1){
     return pow(sum, 0.5);
 }
 
-inline double k1_x(double x){ //x<x0
+inline double k1_x(double x){               // k(x) при x < x0
     return pow(x, 2)+0.5;
 }
 
-inline double k2_x(double x){ //x>x0
+inline double k2_x(double x){               // k(x) при x > x0
     return pow(x, 2)+0.5;
 }
 
-inline double q1_x(double x){ //x<x0
+inline double q1_x(double x){               // q(x) при x < x0
     return exp(-pow(x, 2));
 }
 
-inline double q2_x(double x){ //x>x0
+inline double q2_x(double x){               // q(x) при x > x0
     return 1;
 }
 
-inline double f1_x(double x){ //x<x0
+inline double f1_x(double x){               // f(x) при x < x0
     return cos(x);
 }
 
-inline double f2_x(double x){ //x>x0
+inline double f2_x(double x){               // f(x) при x > x0
     return 1;
 }
 
-inline double a1_x(double x, double h){
+inline double a1_x(double x, double h){                         // коэф. a(x) в методе прогонки при x < x0
     return k1_x(x+h/2);
 }
 
-inline double b1_x(double x, double h){
+inline double b1_x(double x, double h){                         // коэф. b(x) в методе прогонки при x < x0
     return -(k1_x(x+h/2)+k1_x(x-h/2)+q1_x(x)*pow(h, 2));
 }
 
-inline double c1_x(double x, double h){
+inline double c1_x(double x, double h){                         // коэф. c(x) в методе прогонки при x < x0
     return k1_x(x-h/2);
 }
 
-inline double d1_x(double x, double h){
+inline double d1_x(double x, double h){                         // коэф. d(x) в методе прогонки при x < x0
     return -f1_x(x)*pow(h, 2);
 }
 
-inline double a2_x(double x, double h){
+inline double a2_x(double x, double h){                         // коэф. a(x) в методе прогонки при x > x0
     return k2_x(x+h/2);
 }
 
-inline double b2_x(double x, double h){
+inline double b2_x(double x, double h){                         // коэф. b(x) в методе прогонки при x > x0
     return -(k2_x(x+h/2)+k2_x(x-h/2)+q2_x(x)*pow(h, 2));
 }
 
-inline double c2_x(double x, double h){
+inline double c2_x(double x, double h){                         // коэф. c(x) в методе прогонки при x > x0
     return k2_x(x-h/2);
 }
 
-inline double d2_x(double x, double h){
+inline double d2_x(double x, double h){                         // коэф. d(x) в методе прогонки при x > x0
     return -f2_x(x)*pow(h, 2);
 }
 
-inline double a1(double x0){
+inline double a1(double x0){                                    // коэф. a(x0) в методе прогонки для модельной задачи с постоянными коэф. при x < x0
     return k1_x(x0);
 }
 
-inline double b1(double x0, double h){
+inline double b1(double x0, double h){                          // коэф. b(x0) в методе прогонки для модельной задачи с постоянными коэф. при x < x0
     return -2*k1_x(x0)-q1_x(x0)*pow(h, 2);
 }
 
-inline double c1(double x0){
+inline double c1(double x0){                                    // коэф. c(x0) в методе прогонки для модельной задачи с постоянными коэф. при x < x0
     return k1_x(x0);
 }
 
-inline double d1(double x0, double h){
+inline double d1(double x0, double h){                          // коэф. d(x0) в методе прогонки для модельной задачи с постоянными коэф. при x < x0
     return -f1_x(x0)*pow(h, 2);
 }
 
-inline double a2(double x0){
+inline double a2(double x0){                                    // коэф. a(x0) в методе прогонки для модельной задачи с постоянными коэф. при x > x0
     return k2_x(x0);
 }
 
-inline double b2(double x0, double h){
+inline double b2(double x0, double h){                          // коэф. b(x0) в методе прогонки для модельной задачи с постоянными коэф. при x > x0
     return -2*k2_x(x0)-q2_x(x0)*pow(h, 2);
 }
 
-inline double c2(double x0){
+inline double c2(double x0){                                    // коэф. c(x0) в методе прогонки для модельной задачи с постоянными коэф. при x > x0
     return k2_x(x0);
 }
 
-inline double d2(double x0, double h){
+inline double d2(double x0, double h){                          // коэф. d(x0) в методе прогонки для модельной задачи с постоянными коэф. при x > x0
     return -f2_x(x0)*pow(h, 2);
 }
 
-inline double lambda1(double x0){
-    return sqrt(q1_x(x0)/k1_x(x0));
+inline double lambda1(double x0){           // коэф. общего решения вида C*exp(+-lambda1*x) для однородного уравнения
+    return sqrt(q1_x(x0)/k1_x(x0));         // для модельной задачи с постоянными коэф. при x < x0
 }
 
-inline double mu1(double x0){
+inline double mu1(double x0){               // частное решение неоднородного уравнения для модельной задачи с постоянными коэф. при x < x0
     return f1_x(x0)/q1_x(x0);
 }
 
-inline double lambda2(double x0){
-    return sqrt(q2_x(x0)/k2_x(x0));
+inline double lambda2(double x0){           // коэф. общего решения вида C*exp(+-lambda2*x) для однородного уравнения
+    return sqrt(q2_x(x0)/k2_x(x0));         // для модельной задачи с постоянными коэф. при x > x0
 }
 
-inline double mu2(double x0){
+inline double mu2(double x0){               // частное решение неоднородного уравнения для модельной задачи с постоянными коэф. при x > x0
     return f2_x(x0)/q2_x(x0);
 }
 
-void tma_mod(double *u, double h, double x0, int n, double xInit){
-    double alpha[10*n+1];
-    double beta[10*n+1];
+void tma_mod(double *u, double h, double x0, int n, double xInit){          // выч. сеточной ф-ции(tridiagonal matrix algorithm) для модельной зад. с постоянными коэф.
+    double alpha[10*n+1];                   //
+    double beta[10*n+1];                    // 
     double uBuf;
     int la = 0, lb = 0;
     
-    alpha[1] = -a1(x0)/b1(x0, h);
+    alpha[1] = -a1(x0)/b1(x0, h);                           //
     beta[1] = (d1(x0, h)-c1(x0)*u[0])/b1(x0, h);
     alpha[10*n-1] = -c2(x0)/b2(x0, h);
     beta[10*n-1] = (d2(x0, h)-c2(x0)*u[10])/b2(x0, h);
     
-    for (int l = 1; xInit+l*h < x0; l++){
+    for (int l = 1; xInit+l*h < x0; l++){       // поиск точки la до точки разрыва; да, лучше было просто поделить и привести к целому типу
         la += 1;
     }
     
-    lb = la+1;
+    lb = la+1;                                  // точка lb с другой стороны от разрыва
     
-    for (int l = 2; l < la; l++){
+    for (int l = 2; l < la; l++){                                                   // прогонка от 0 до la
         alpha[l] = -a1(x0)/(b1(x0, h)+c1(x0)*alpha[l-1]);
         beta[l] = (d1(x0, h)-c1(x0)*beta[l-1])/(b1(x0, h)+c1(x0)*alpha[l-1]);
     }
     
-    for (int l = 10*n-2; l > lb; l--){
+    for (int l = 10*n-2; l > lb; l--){                                              // прогонка от 1 до lb
         alpha[l] = -c2(x0)/(b2(x0, h)+a2(x0)*alpha[l+1]);
         beta[l] = (d2(x0, h)-a2(x0)*beta[l+1])/(b2(x0, h)+c2(x0)*alpha[l+1]);
     }
     
-    uBuf = (k1_x(x0)*beta[la-1]+k2_x(x0)*beta[lb+1])/(k1_x(x0)*(1-alpha[la-1])+k2_x(x0)*(1-alpha[lb+1])); //u(la)
+    uBuf = (k1_x(x0)*beta[la-1]+k2_x(x0)*beta[lb+1])/(k1_x(x0)*(1-alpha[la-1])+k2_x(x0)*(1-alpha[lb+1]));   // u(la)
     
-    for (int l = la; l >= 2; l--){
+    for (int l = la; l >= 2; l--){                                                  // обратная прогонка от u(la) до u(0+0)
         uBuf = alpha[l-1]*uBuf+beta[l-1];
         
-        if ((l-1)%n == 0){
+        if ((l-1)%n == 0){                                                          // запись одной из 11-ти точек
             u[(l-1)/n] = uBuf;
         }
     }
     
-    uBuf = (k1_x(x0)*beta[la-1]+k2_x(x0)*beta[lb+1])/(k1_x(x0)*(1-alpha[la-1])+k2_x(x0)*(1-alpha[lb+1])); //u(lb)
+    uBuf = (k1_x(x0)*beta[la-1]+k2_x(x0)*beta[lb+1])/(k1_x(x0)*(1-alpha[la-1])+k2_x(x0)*(1-alpha[lb+1]));   // u(lb)
     
-    for (int l = lb; l <= 10*n-2; l++){
+    for (int l = lb; l <= 10*n-2; l++){                                             // обратная прогонка от u(lb) до (1-0)
         uBuf = alpha[l+1]*uBuf+beta[l+1];
         
-        if ((l+1)%n == 0){
+        if ((l+1)%n == 0){                                                          // запись одной из 11-ти точек
             u[(l+1)/n] = uBuf;
         }
     }
 }
 
-void tma(double *u, double h, double x0, int n, double xInit){
+void tma(double *u, double h, double x0, int n, double xInit){              // вычисление сеточной ф-ции (tridiagonal matrix algorithm) для основной зад.
     double alpha[10*n+1];
     double beta[10*n+1];
     double uBuf;
@@ -207,11 +207,11 @@ void tma(double *u, double h, double x0, int n, double xInit){
     alpha[10*n-1] = -c2_x(xInit+h*(10*n-1), h)/b2_x(xInit+h*(10*n-1), h);
     beta[10*n-1] = (d2_x(xInit+h*(10*n-1), h)-c2_x(xInit+h*(10*n-1), h)*u[10])/b2_x(xInit+h*(10*n-1), h);
     
-    for (int l = 1; xInit+l*h < x0; l++){
+    for (int l = 1; xInit+l*h < x0; l++){       // поиск точки la до точки разрыва; да, лучше было просто поделить и привести к целому типу
         la += 1;
     }
     
-    lb = la+1;
+    lb = la+1;                                  // точка lb с другой стороны от разрыва
     
     for (int l = 2; l < la; l++){
         alpha[l] = -a1_x(xInit+h*l, h)/(b1_x(xInit+h*l, h)+c1_x(xInit+h*l, h)*alpha[l-1]);
@@ -223,7 +223,7 @@ void tma(double *u, double h, double x0, int n, double xInit){
         beta[l] = (d2_x(xInit+h*l, h)-a2_x(xInit+h*l, h)*beta[l+1])/(b2_x(xInit+h*l, h)+c2_x(xInit+h*l, h)*alpha[l+1]);
     }
     
-    uBuf = (k1_x(xInit+h*la)*beta[la-1]+k2_x(xInit+h*lb)*beta[lb+1])/(k1_x(xInit+h*la)*(1-alpha[la-1])+k2_x(xInit+h*lb)*(1-alpha[lb+1])); //u(la)
+    uBuf = (k1_x(xInit+h*la)*beta[la-1]+k2_x(xInit+h*lb)*beta[lb+1])/(k1_x(xInit+h*la)*(1-alpha[la-1])+k2_x(xInit+h*lb)*(1-alpha[lb+1]));   // u(la)
     
     for (int l = la; l >= 2; l--){
         uBuf = alpha[l-1]*uBuf+beta[l-1];
@@ -233,7 +233,7 @@ void tma(double *u, double h, double x0, int n, double xInit){
         }
     }
     
-    uBuf = (k1_x(xInit+h*la)*beta[la-1]+k2_x(xInit+h*lb)*beta[lb+1])/(k1_x(xInit+h*la)*(1-alpha[la-1])+k2_x(xInit+h*lb)*(1-alpha[lb+1])); //u(lb)
+    uBuf = (k1_x(xInit+h*la)*beta[la-1]+k2_x(xInit+h*lb)*beta[lb+1])/(k1_x(xInit+h*la)*(1-alpha[la-1])+k2_x(xInit+h*lb)*(1-alpha[lb+1]));   // u(lb)
     
     for (int l = lb; l <= 10*n-2; l++){
         uBuf = alpha[l+1]*uBuf+beta[l+1];
--- program_1/main.cpp
+++ program_1/main.cpp
@@ -16,19 +16,19 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    double xInit = 0;
-    double xEnd = 1;
-    double x0 = 1/sqrt(2);
-    double e = 0.01;
+    double xInit = 0;                                   // начало отрезка
+    double xEnd = 1;                                    // конец отрезка
+    double x0 = 1/sqrt(2);                              // точка разрыва
+    double e = 0.0001;                                  // относительная погрешность
     double u_mod[11], u1_mod[11], u2_mod[11];
     double u1[11], u2[11];
-    double h = (xEnd-xInit)/10;
-    double hConst = h;
+    double h = (xEnd-xInit)/10;                         // начальное значение шага вычислений (отрезок делится на 10 частей, чтобы посчитать значения ф-ции в 11 точках)
+    double hConst = h;                                  // шаг для модельной задачи с постоянными коэффициентами
     u_mod[0] = 0; u1_mod[0] = 0; u2_mod[0] = 0;
     u_mod[10] = 1; u1_mod[10] = 1; u2_mod[10] = 1;
     u1[0] = 0; u2[0] = 0;
     u1[10] = 1; u2[10] = 1;
-    int n = 1;
+    int n = 1;                                          // отрезок при вычислениях делится на 10*n частей
 //    cout.setf(ios::scientific);
     
     for (int l = 1; l < 10; l++){
@@ -38,7 +38,7 @@ int main(int argc, const char * argv[]) {
         u2[l] = 1;
     }
     
-    while ((norm1(u2_mod, u1_mod)/1) > e){
+    while ((norm1(u2_mod, u1_mod)/1) > e){       // численное решение модельной задачи; магическое число 1 = xEnd-xInit
         tma_mod(u2_mod, h, x0, n, xInit);
         tma_mod(u1_mod, h/2, x0, n*2, xInit);
         
@@ -46,7 +46,7 @@ int main(int argc, const char * argv[]) {
         n *= 2;
     }
     
-    double A11, A12, A21, A22;
+    double A11, A12, A21, A22;                          // аналитическое решение модельной задачи
     double B1, B2;
     double C1, C2, C3, C4;
     
@@ -56,18 +56,18 @@ int main(int argc, const char * argv[]) {
     A22 = k2_x(x0)*lambda2(x0)*(exp(lambda2(x0)*(2-x0))+exp(lambda2(x0)*x0));
     B1 = mu2(x0)-mu1(x0)+(mu1(x0)-u_mod[0])*exp(lambda1(x0)*x0)-(mu2(x0)-u_mod[10])*exp(lambda2(x0)*(1-x0));
     B2 = k1_x(x0)*lambda1(x0)*(u_mod[0]-mu1(x0))*exp(lambda1(x0)*x0)+k2_x(x0)*lambda2(x0)*(u_mod[10]-mu2(x0))*exp(lambda2(x0)*(1-x0));
-    C1 = (((u_mod[0]-mu1(x0))*A11-B1)*A22-((u_mod[0]-mu1(x0))*A21-B2)*A12)/(A11*A22-A12*A21);
-    C2 = (B1*A22-B2*A12)/(A11*A22-A12*A21);
-    C3 = (B2*A11-B1*A21)/(A11*A22-A12*A21);
+    C1 = (((u_mod[0]-mu1(x0))*A11-B1)*A22-((u_mod[0]-mu1(x0))*A21-B2)*A12)/(A11*A22-A12*A21);               // C1, C2 - коэф. общего решения
+    C2 = (B1*A22-B2*A12)/(A11*A22-A12*A21);                                                                 // C4, C3 - коэф. общего решения
+    C3 = (B2*A11-B1*A21)/(A11*A22-A12*A21);                                                                 // C1..C4 вычисляются по ф-ле (7) со стр. 68
     C4 = (u_mod[10]-mu2(x0))*exp(lambda2(x0))-C3*exp(2*lambda2(x0));
     
-    for (int l = 1; l <= 7; l++){ //1/sqrt(2) ~= 0.7071
-        u_mod[l] = C1*exp(lambda1(x0)*(xInit+hConst*l))+C2*exp(-lambda1(x0)*(xInit+hConst*l))+mu1(x0);
-    }
+    for (int l = 1; l <= 7; l++){   // 1/sqrt(2) ~= 0.7071 > 7
+        u_mod[l] = C1*exp(lambda1(x0)*(xInit+hConst*l))+C2*exp(-lambda1(x0)*(xInit+hConst*l))+mu1(x0);      // аналитическое реш. модельной зад. при x < x0
+    }                                                                                                       // u = u_общее + u_частное
     
-    for (int l = 9; l >= 8; l--){
-        u_mod[l] = C3*exp(lambda2(x0)*(xInit+hConst*l))+C4*exp(-lambda2(x0)*(xInit+hConst*l))+mu2(x0);
-    }
+    for (int l = 9; l >= 8; l--){   // 1/sqrt(2) ~= 0.7071 < 8
+        u_mod[l] = C3*exp(lambda2(x0)*(xInit+hConst*l))+C4*exp(-lambda2(x0)*(xInit+hConst*l))+mu2(x0);      // аналитическое реш. модельной зад. при x > x0
+    }                                                                                                       // u = u_общее + u_частное
     
     cout << "Model task solution:" << endl;
     
@@ -79,7 +79,7 @@ int main(int argc, const char * argv[]) {
     h = hConst;
     n = 1;
     
-    while ((norm1(u2, u1)/1) > e){
+    while ((norm1(u2, u1)/1) > e){               // численное решение основной задачи; магическое число 1 = xEnd-xInit
         tma(u2, h, x0, n, xInit);
         tma(u1, h/2, x0, n*2, xInit);
         
